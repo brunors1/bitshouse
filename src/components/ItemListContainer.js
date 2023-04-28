ef22c349fb54a476443fa49b682a42a4770fbf8a
Bruno Santos <bruno_reworks@outlook.com>
2023-04-28 12:03:49 -0300

    ItemCount

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,20 +1,28 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import ItemCount from "./ItemCount";
 
 const Greetings = ({texto}) => {
-    return (
-        <h1 className="text-center">{texto}</h1>
-    )
+  return (
+    <h1 className="text-center">{texto}</h1>
+  )
 }
 
 function ItemListContainer() {
-    return (
-       <div className="container">
-            <section className="bg-white">
-               <Greetings texto="Olá, Seja Bem Vindo(a) a minha futura loja virtual!"/>
-               <Greetings texto="Aqui teremos diversos hardwares e componentes de computador."/>
-            </section>
-       </div>
-    );
+  const handleAdd = (count) => {
+    console.log(`Adicionar ${count} itens ao carrinho`);
+  }
+
+  return (
+    <div className="container">
+      <section className="bg-white">
+        <Greetings texto="Olá, Seja Bem Vindo(a) a minha futura loja virtual!"/>
+        <Greetings texto="Aqui teremos diversos hardwares e componentes de computador."/>
+      </section>
+      <section>
+        <ItemCount stock={10} initial={1} onAdd={handleAdd}/>
+      </section>
+    </div>
+  );
 }
 
 export default ItemListContainer;
