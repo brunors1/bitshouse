4c431799c90eab7ecb98d2f18943c262148966e3
Bruno Santos <bruno_reworks@outlook.com>
2023-05-04 15:39:41 -0300

    Correções Itemlist e Item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 
-function Item ({id, title, price, pictureUrl, description}) {
+function Item ({id, title, price, pictureUrl, buttonLabel}) {
     return (
-            <div className="col">
-                <div className="card h-100" key={id}>
+            <div className="col" key={id}>
+                <div className="card h-100">
                     <img className='card-img-top' src={pictureUrl} alt={title} />
                     <div className="card-body mx-auto text-center">
                         <h2 className="card-title">{title}</h2>
                         <h4 className="card-text">{price}</h4>
-                        <button type="button" className="btn btn-success">{description}</button>
+                        <button type="button" className="btn btn-success">{buttonLabel}</button>
                     </div>
                 </div>
             </div>
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import Item from './Item';
 
+const mockData = [
+  {id: 1,title: 'Item 1',buttonLabel: 'Detalhes',price: 'R$: 300,00',pictureUrl: 'https://picsum.photos/id/1/200/200'},
+  {id: 2,title: 'Item 2',buttonLabel: 'Detalhes',price: 'R$: 100,00',pictureUrl: 'https://picsum.photos/id/2/200/200'},
+  {id: 3,title: 'Item 3',buttonLabel: 'Detalhes',price: 'R$: 600,00',pictureUrl: 'https://picsum.photos/id/3/200/200'},
+  {id: 4,title: 'Item 4',buttonLabel: 'Detalhes',price: 'R$: 450,00',pictureUrl: 'https://picsum.photos/id/4/200/200'},
+  {id: 5,title: 'Item 5',buttonLabel: 'Detalhes',price: 'R$: 230,00',pictureUrl: 'https://picsum.photos/id/5/200/200'},
+  {id: 6,title: 'Item 6',buttonLabel: 'Detalhes',price: 'R$: 870,00',pictureUrl: 'https://picsum.photos/id/6/200/200'},
+  {id: 7,title: 'Item 7',buttonLabel: 'Detalhes',price: 'R$: 320,00',pictureUrl: 'https://picsum.photos/id/7/200/200'},
+  {id: 8,title: 'Item 8',buttonLabel: 'Detalhes',price: 'R$: 700,00',pictureUrl: 'https://picsum.photos/id/8/200/200'}
+];
+
 function ItemList() {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    const mockData = [
-      {id: 1,title: 'Item 1',description: 'Detalhes',price: 'R$: 300,00',pictureUrl: 'https://picsum.photos/id/1/200/200'},
-      {id: 2,title: 'Item 2',description: 'Detalhes',price: 'R$: 100,00',pictureUrl: 'https://picsum.photos/id/2/200/200'},
-      {id: 3,title: 'Item 3',description: 'Detalhes',price: 'R$: 600,00',pictureUrl: 'https://picsum.photos/id/3/200/200'},
-      {id: 4,title: 'Item 4',description: 'Detalhes',price: 'R$: 450,00',pictureUrl: 'https://picsum.photos/id/4/200/200'},
-      {id: 5,title: 'Item 5',description: 'Detalhes',price: 'R$: 230,00',pictureUrl: 'https://picsum.photos/id/5/200/200'},
-      {id: 6,title: 'Item 6',description: 'Detalhes',price: 'R$: 870,00',pictureUrl: 'https://picsum.photos/id/6/200/200'},
-      {id: 7,title: 'Item 7',description: 'Detalhes',price: 'R$: 320,00',pictureUrl: 'https://picsum.photos/id/7/200/200'},
-      {id: 8,title: 'Item 8',description: 'Detalhes',price: 'R$: 700,00',pictureUrl: 'https://picsum.photos/id/8/200/200'}
-    ];
-
     const fetchItems = async () => {
       return mockData;
     };
@@ -28,10 +28,10 @@ function ItemList() {
   return (
     <div className="row row-cols-1 row-cols-md-4 g-4">
       {items.map((item) => (
-        <Item id={item.id} title={item.title} description={item.description} price={item.price} pictureUrl={item.pictureUrl}/>
+        <Item key={item.id} id={item.id} title={item.title} buttonLabel={item.buttonLabel} price={item.price} pictureUrl={item.pictureUrl}/>
       ))}
     </div>
-  )   
+  );
 }
 
 export default ItemList;
